04f15c68951060ce537d7f1aff3ddeee50c49485
gjmrd <magomedovgh@gmail.com>
2019-09-02 11:21:40 +0300

    encapsulated main logic in different method

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -7,16 +7,19 @@ public class Task2 {
             System.out.println("missing argument");
             return;
         }
+       printNumbersFor(args[0]);
+    }
+
+    public static void printNumbersFor(String name) {
         PhoneBook phoneBook = PhoneBook.getInstance();
-        String input = args[0];
-        ArrayList<String> phoneNumbers = phoneBook.getPhoneNumbers().get(input);
+        ArrayList<String> phoneNumbers = phoneBook.getPhoneNumbers().get(name);
 
         if (phoneNumbers != null && phoneNumbers.size() > 0) {
             for (int index = 1; index <= phoneNumbers.size(); index++) {
                 System.out.println(String.format("%d. %s", index, phoneNumbers.get(index - 1)));
             }
         } else {
-            System.out.println("No numbers for " + input);
+            System.out.println("No numbers for " + name);
         }
     }
 }
