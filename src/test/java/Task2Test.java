7f2b44cf10fdd214a8f23990492511ff51d518f4
gjmrd <magomedovgh@gmail.com>
2019-09-02 10:02:02 +0300

    fixed issue with line separators in linux

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -8,6 +8,7 @@ public class Task2Test {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
+    private final String lineSeparator = System.getProperty("line.separator");
 
     @Before
     public void setUpStreams() {
@@ -23,21 +24,21 @@ public class Task2Test {
     public void main() {
         String[] args = new String[] {"Иванов И.И."};
         Task2.main(args);
-        Assert.assertEquals("1. +8 800 2000 500\r\n2. +8 800 200 600\r\n", outContent.toString());
+        Assert.assertEquals(String.format("1. +8 800 2000 500%s2. +8 800 200 600%s", lineSeparator, lineSeparator), outContent.toString());
     }
 
     @Test
     public void mainWithNoNumbers() {
         String[] args = new String[] {"Кабачков К.К."};
         Task2.main(args);
-        Assert.assertEquals("No numbers for Кабачков К.К.\r\n", outContent.toString());
+        Assert.assertEquals(String.format("No numbers for Кабачков К.К.%s", lineSeparator), outContent.toString());
     }
 
     @Test
     public void mainWithNoArgs() {
         String[] args = new String[]{};
         Task2.main(args);
-        Assert.assertEquals("missing argument\r\n", outContent.toString());
+        Assert.assertEquals(String.format("missing argument%s", lineSeparator), outContent.toString());
     }
 
 }
